9b52453f587fba3cb6ee9c938760123c71d4ab2d
Ian Armour <ianmarmour@gmail.com>
2020-03-28 21:49:18 -0700

    Basic app version working,

--- App.tsx
+++ App.tsx
@@ -1,30 +1,52 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import Plant from "./components/plant/plant";
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from '@apollo/react-hooks';
+import * as Font from 'expo-font';
 
-export default function App() {
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const cache = new InMemoryCache()
+
+const client: any = new ApolloClient({link: httpLink, cache: cache});
+
+const App = () => {
+  Font.loadAsync({
+    'BebasNeue': require('./assets/fonts/BebasNeue-Regular.ttf'),
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Muck Moisture Tracker</Text>
+    <ApolloProvider client={client}>
+      <View style={styles.container}><Text style={styles.header}>Muck Moisture Tracker</Text>
       <Plant />
-    </View>
-  );
+      </View>
+    </ApolloProvider>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "flex-start"
+    alignItems: 'center',
   },
   logo: {
     width: 66,
-    height: 58
+    height: 58,
   },
   header: {
+    paddingLeft: 10,
+    paddingRight: 10,
     fontSize: 50,
-    fontFamily: "BebasNeue-Bold",
-    color: "#282a2c"
+    fontFamily: "BebasNeue",
+    backgroundColor: "#D3EEDD",
+    borderColor: 'lightgrey',
+    borderBottomWidth: 1,
   }
 });
+
+export default App
--- components/plant/plant.tsx
+++ components/plant/plant.tsx
@@ -1,26 +1,80 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
+import { useQuery } from '@apollo/react-hooks';
+import gql from  'graphql-tag';  
+
+const FETCH_ALL_PLANTS = gql `query {  
+  plants {
+    name
+    soil {
+      moistureLevel
+      brand
+    }
+  }  
+}  
+`;  
 
 export default function Plant() {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.logo} source={require("../../assets/plant.png")} />
-      <Text>Name: </Text>
-      <Text>Moisture Level:</Text>
-    </View>
+  const { loading, data } = useQuery<any, any>(
+    FETCH_ALL_PLANTS
   );
+
+  return (
+    <View style={styles.contentContainer}>
+    {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        data.plants.map(plant => (
+          <View style={styles.plantContainer}>
+            <View style ={styles.plantHeader}>
+              <Image style={styles.logo} source={require("../../assets/plant.png")} />
+            </View>
+            <View style ={styles.plantDetails}>
+              <Text style={styles.plantKey}>Name:</Text>
+              <Text style={styles.plantValue}>{plant.name}</Text>
+            </View>
+            <View style ={styles.plantDetails}>
+              <Text style={styles.plantKey}>Soil Moisture:</Text>
+              <Text style={styles.plantValue}>{plant.soil.moistureLevel}</Text>
+            </View> 
+            <View style ={styles.plantDetails}>
+              <Text style={styles.plantKey}>Soil Brand:</Text>
+              <Text style={styles.plantValue}>{plant.soil.brand}</Text>
+            </View> 
+          </View>
+        )))}
+  </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  plantContainer: {
     alignItems: "center",
-    justifyContent: "flex-start"
+    borderColor: 'lightgrey',
+    borderBottomWidth: 1,
+    marginTop: 10
+  },
+  plantHeader: {
+    alignItems: "center"
+  },
+  plantDetails: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  plantKey: {
+    fontWeight: 'bold',
+    width: 130,
+    marginLeft: 10
+  },
+  plantValue: {
+    fontWeight: 'normal'
   },
   logo: {
     width: 66,
-    height: 58
+    height: 58,
   },
   header: {
     fontSize: 50,
